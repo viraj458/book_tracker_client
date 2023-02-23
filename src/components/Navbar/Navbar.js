b4ae92a9f03b1953a11c2a063aac308c02faa9ac
viraj458 <virajmalaka94@gmail.com>
2023-02-23 17:20:58 +0530

    updated Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,55 +1,16 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import { Link } from 'react-router-dom';
 
-
-function Navbar() {
+const Navbar = () => {
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'Poppins',
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Book Tracker
-          </Typography>
-
-          <MenuBookIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'Poppins',
-              fontWeight: 700,
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Book Tracker
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Bookmark</h1>
+        </Link>
+      </div>
+    </header>
+  )
 }
-export default Navbar
+
+export default Navbar;
