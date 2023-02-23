febf42261ed13e7479daba802bd34769f08b31bd
viraj458 <virajmalaka94@gmail.com>
2023-02-23 23:45:32 +0530

    fetched book data from api

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
+import BookDetails from "../../components/BookDetails/BookDetails"
 
 const Home = () => {
+const [books, setBooks] = useState(null)
+
+    useEffect(()=>{
+        const fetchBooks = async() => {
+            const response = await fetch('/api/books')
+            const data = await response.json()
+
+            if(response.ok){
+                console.log(data)
+                setBooks(data)
+            }
+
+        }
+        fetchBooks()
+
+    },[])
 
     return(
         <div className="home">
-            Home
+            <div className="books">
+            {books && books.map((book)=>(
+                <BookDetails book={book}  key={book._id}/>
+            ))}
+            </div>
         </div>
     )
 }
